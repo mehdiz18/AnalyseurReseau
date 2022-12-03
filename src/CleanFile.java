e9a86c5121ce26572a6cb78154969016e8d844e9
thunder1806 <zeninemehdi@gmail.com>
2022-12-03 23:34:25 +0100

    resolved not recognizing text

--- src/CleanFile.java
+++ src/CleanFile.java
@@ -18,7 +18,6 @@ public class CleanFile {
             BufferedWriter bWriter = new BufferedWriter(new FileWriter(cleanFile));
             String st;
             String[] stTab;
-            int i = 1;
             while ((st = bf.readLine()) != null) {
                 // Sauter les lignes vides
                 if (st.isBlank()) {
@@ -27,14 +26,15 @@ public class CleanFile {
                     String cleanString = "";
                     // Je split sur 2 espaces ou plus
                     stTab = st.split("  +");
-                    for (int j = 1; j < stTab.length; j++) {
-                        if (stTab[j].matches("^[0-9a-fA-F ]+$")) {
-                            cleanString += stTab[j];
+                    if (stTab.length >= 2) {
+                        for (int j = 1; j < stTab.length; j++) {
+                            if (stTab[j].matches("^[0-9a-fA-F ]+$")) {
+                                cleanString += stTab[j];
+                            }
                         }
+                        bWriter.write(cleanString + " ", 0, cleanString.length() + 1);
                     }
-                    bWriter.write(cleanString + " ", 0, cleanString.length() + 1);
                 }
-                i++;
             }
             bf.close();
             bWriter.close();
